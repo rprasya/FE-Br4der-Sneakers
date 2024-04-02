ccea8cdd2d204953d7e9dea58166335976abc14a
Rinaldi Prasya <103562307+rprasya@users.noreply.github.com>
2024-04-02 16:27:51 +0700

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -66,6 +66,35 @@ const Home = () => {
         </div>
       </div>
       {/* slider end */}
+
+      {/* popularity */}
+      <div className="flex flex-col justify-center items-center border border-red-600">
+        <h1 className="font-bold text-2xl my-5 mr-[860px]">Populer hari ini</h1>
+        <ul className="flex border border-black gap-8">
+          <li className="w-[300px] border border-blue-800">
+            <img
+              src="https://dynamic.zacdn.com/jPqdE4KAhoq8gsJ1AJ9r4bG2FdQ=/filters:quality(70):format(webp)/https://static-id.zacdn.com/p/nike-7594-1884434-1.jpg"
+              alt=""
+              className=""
+            />
+          </li>
+          <li className="w-[300px] border border-blue-800">
+            <img
+              src="https://cdn-images.farfetch-contents.com/22/28/58/43/22285843_52098863_1000.jpg"
+              alt=""
+              className=""
+            />
+          </li>
+          <li className="w-[300px] border border-blue-800">
+            <img
+              src="https://cdn-images.farfetch-contents.com/18/78/65/19/18786519_40721703_1000.jpg"
+              alt=""
+              className=""
+            />
+          </li>
+        </ul>
+      </div>
+      {/* popularity end */}
     </>
   );
 };

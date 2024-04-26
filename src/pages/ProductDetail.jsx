ce5142d0df100590a29f7bd9ce27c149e16f11da
Rinaldi Prasya <103562307+rprasya@users.noreply.github.com>
2024-04-26 14:09:28 +0700

    update product detail page, add format price IDR

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,51 +1,59 @@
-import { useState ,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
-    const params = useParams()
-    const [product, setProduct] = useState({})
-    const [loading, setLoading] = useState(true)
-    const [notFound, setNotFound] = useState(false)
+  const params = useParams();
+  const [product, setProduct] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [notFound, setNotFound] = useState(false);
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-            try {
-                const response = await fetch(`https://br4der-api.up.railway.app/product/${params.id}`);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(
+          `https://br4der-api.up.railway.app/product/${params.id}`
+        );
 
-                if (!response.ok) {
-                    return setNotFound(true)
-                }
-
-                const getData = await response.json();
-                
-                setProduct(getData)
-                setLoading(false)
-            } catch (error) {
-                console.log("Fetch Error:", error)
-            }
+        if (!response.ok) {
+          return setNotFound(true);
         }
-        fetchProduct()
-    }, [params])
 
-    if(notFound) {
-        return (
-            <h1 className="font-bold">Product is Not Found</h1>
-        )
-    }
+        const getData = await response.json();
 
-    return(
-        <section className="text-center">
-            {loading ? (
-                <i>Loading Product...</i>
-            ) : (
-                <div>
-                    <img src={product.imageUrl} alt={product.name} className="w-96" />
-                    <p>{product.name}</p>
-                    <p>{product.price}</p>
-                </div>
-            )}
-        </section>
-    )
-}
+        setProduct(getData);
+        setLoading(false);
+      } catch (error) {
+        console.log("Fetch Error:", error);
+      }
+    };
+    fetchProduct();
+  }, [params]);
+
+  if (notFound) {
+    return <h1 className="font-bold">Product is Not Found</h1>;
+  }
+
+  const formatPrice = (price) => {
+    return price.toLocaleString("id-ID", {
+      style: "currency",
+      currency: "IDR",
+      minimumFractionDigits: 0 // memastikan bahwa tdk ada digit pecahan (koma) di belakang angka 0
+    });
+  };
+
+  return (
+    <section className="text-center">
+      {loading ? (
+        <i>Loading Product...</i>
+      ) : (
+        <div>
+          <img src={product.imageUrl} alt={product.name} className="w-96" />
+          <p>{product.name}</p>
+          <p>{formatPrice(product.price)}</p>
+        </div>
+      )}
+    </section>
+  );
+};
 
 export default ProductDetail;

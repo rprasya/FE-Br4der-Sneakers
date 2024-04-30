60957d7eeb0b69e1d676ae77cede1991b52bd8d1
Rinaldi Prasya <103562307+rprasya@users.noreply.github.com>
2024-04-30 15:39:01 +0700

    update detail product and footer component

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const ProductDetail = () => {
   const params = useParams();
   const [product, setProduct] = useState({});
   const [loading, setLoading] = useState(true);
   const [notFound, setNotFound] = useState(false);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -37,7 +39,7 @@ const ProductDetail = () => {
     return price.toLocaleString("id-ID", {
       style: "currency",
       currency: "IDR",
-      minimumFractionDigits: 0 // memastikan bahwa tdk ada digit pecahan (koma) di belakang angka 0
+      minimumFractionDigits: 0, // memastikan bahwa tdk ada digit pecahan (koma) di belakang angka 0
     });
   };
 
@@ -47,9 +49,82 @@ const ProductDetail = () => {
         <i>Loading Product...</i>
       ) : (
         <div>
-          <img src={product.imageUrl} alt={product.name} className="w-96" />
-          <p>{product.name}</p>
-          <p>{formatPrice(product.price)}</p>
+          <div className="flex justify-between items-center mb-10">
+            <div className="w-[707px] flex flex-col justify-center items-center">
+              <img src={product.imageUrl} alt={product.name} className="w-96" />
+              <p>{product.name}</p>
+              <p>{formatPrice(product.price)}</p>
+            </div>
+            <div className="w-[700px] h-[497px] flex flex-col justify-center items-center">
+              <div className="text-left">
+                <p className="text-xl font-bold mb-6">Information Sneaker</p>
+                <p className="mb-16">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                  debitis cum rem maiores nemo non, a aperiam veritatis porro
+                  ipsam ipsa cumque fugit dolor ducimus quisquam rerum adipisci,
+                  alias corrupti.
+                </p>
+                <p className="text-xl font-bold mb-5">Size Sneaker</p>
+                <ul className="flex gap-2 mb-16">
+                  <li className="w-9 text-center p-1 border border-solid border-red-600 rounded-md hover:text-white hover:bg-red-600 hover:cursor-pointer">
+                    38
+                  </li>
+                  <li className="w-9 text-center p-1 border border-solid border-red-600 rounded-md hover:text-white hover:bg-red-600 hover:cursor-pointer">
+                    39
+                  </li>
+                  <li className="w-9 text-center p-1 border border-solid border-red-600 rounded-md hover:text-white hover:bg-red-600 hover:cursor-pointer">
+                    40
+                  </li>
+                  <li className="w-9 text-center p-1 border border-solid border-red-600 rounded-md hover:text-white hover:bg-red-600 hover:cursor-pointer">
+                    41
+                  </li>
+                  <li className="w-9 text-center p-1 border border-solid border-red-600 rounded-md hover:text-white hover:bg-red-600 hover:cursor-pointer">
+                    42
+                  </li>
+                  <li className="w-9 text-center p-1 border border-solid border-red-600 rounded-md hover:text-white hover:bg-red-600 hover:cursor-pointer">
+                    43
+                  </li>
+                  <li className="w-9 text-center p-1 border border-solid border-red-600 rounded-md hover:text-white hover:bg-red-600 hover:cursor-pointer">
+                    44
+                  </li>
+                  <li className="w-9 text-center p-1 border border-solid border-red-600 rounded-md hover:text-white hover:bg-red-600 hover:cursor-pointer">
+                    45
+                  </li>
+                </ul>
+                <p className="text-xl font-bold mb-5">Atur Jumlah</p>
+                <div className="flex">
+                  <div className="flex text-center mr-8 gap-3">
+                    <button
+                      className="text-red-600 px-3 rounded-lg hover:bg-red-600 hover:text-white"
+                      onClick={() => {
+                        setCount(count - 1);
+                      }}
+                    >
+                      -
+                    </button>
+                    <p>{count}</p>
+                    <button
+                      className="text-red-600 px-3 rounded-lg hover:bg-red-600 hover:text-white"
+                      onClick={() => {
+                        setCount(count + 1);
+                      }}
+                    >
+                      +
+                    </button>
+                  </div>
+                  <div className="flex gap-4">
+                    <button className="text-red-600 px-6 border border-solid border-red-600 rounded-lg hover:text-white hover:bg-red-600">
+                      Add Cart
+                    </button>
+                    <button className="text-red-600 px-6 border border-solid border-red-600 rounded-lg hover:text-white hover:bg-red-600">
+                      Checkout
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Footer />
         </div>
       )}
     </section>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const Product = () => {
   const [products, setProducts] = useState([]);
@@ -24,30 +25,73 @@ const Product = () => {
   }, []);
 
   return (
-    <section className="text-center">
-      <div className="font-bold text-3xl my-5">All Sneakers</div>
+    <section className="">
+      <div className="flex font-semibold text-lg mx-8 my-5">
+        <Link to="/" className="hover:underline">
+          Home
+        </Link>
+        <p className="mx-2">&gt;</p>
+        <Link to="/products" className="text-red-600 hover:underline">
+          Products
+        </Link>
+      </div>
       {loading ? (
         <i>Loading Products...</i>
       ) : (
-        <div className="flex flex-col min-h-[500px] items-center justify-center mt-16 gap-16">
-          <div className="grid grid-cols-1 md:grid-cols-4 lg:cols-3 gap-16">
-            {products.map((product) => {
-              return (
-                <div key={product.id}>
-                  <img
-                    src={product.imageUrl}
-                    alt={product.name}
-                    className="w-96"
-                  />
-                  <h2 className="text-black hover:underline">
-                    <Link to={`/products/${product.id}`}>{product.name}</Link>
-                  </h2>
-                </div>
-              );
-            })}
+        <div className="flex">
+          <div className="w-72 h-80 mt-6 flex justify-center">
+            <div className="h-full w-48 flex flex-col text-center rounded-lg border border-solid border-red-700">
+              <p className="font-semibold mt-3 mb-4">Select Brand</p>
+              <ul>
+                <li className="my-5 text-center">
+                  <Link to="" className="px-11 py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600">
+                    Adidas
+                  </Link>
+                </li>
+                <li className="my-5">
+                  <Link to="" className="px-[52px] py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600">
+                    Nike
+                  </Link>
+                </li>
+                <li className="my-5">
+                  <Link to="" className="px-6 py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600">
+                    New Balance
+                  </Link>
+                </li>
+                <li className="my-5">
+                  <Link to="" className="px-[52px] py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600">
+                    Asics
+                  </Link>
+                </li>
+                <li className="my-5">
+                  <Link to="" className="px-[52px] py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600">
+                    Puma
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex flex-col min-h-[500px] items-center justify-center mt-16 gap-16">
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:cols-3 gap-16">
+              {products.map((product) => {
+                return (
+                  <div key={product.id}>
+                    <img
+                      src={product.imageUrl}
+                      alt={product.name}
+                      className="w-96"
+                    />
+                    <h2 className="text-black hover:underline">
+                      <Link to={`/products/${product.id}`}>{product.name}</Link>
+                    </h2>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       )}
+      <Footer />
     </section>
   );
 };

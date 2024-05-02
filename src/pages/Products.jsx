08880dcb72a07e97a3032dcc0d37a625d3a9393d
Rinaldi Prasya <103562307+rprasya@users.noreply.github.com>
2024-05-02 09:54:13 +0700

    update product page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -44,27 +44,42 @@ const Product = () => {
               <p className="font-semibold mt-3 mb-4">Select Brand</p>
               <ul>
                 <li className="my-5 text-center">
-                  <Link to="" className="px-11 py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600">
+                  <Link
+                    to=""
+                    className="px-11 py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600"
+                  >
                     Adidas
                   </Link>
                 </li>
                 <li className="my-5">
-                  <Link to="" className="px-[52px] py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600">
+                  <Link
+                    to=""
+                    className="px-[52px] py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600"
+                  >
                     Nike
                   </Link>
                 </li>
                 <li className="my-5">
-                  <Link to="" className="px-6 py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600">
+                  <Link
+                    to=""
+                    className="px-6 py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600"
+                  >
                     New Balance
                   </Link>
                 </li>
                 <li className="my-5">
-                  <Link to="" className="px-[52px] py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600">
+                  <Link
+                    to=""
+                    className="px-[52px] py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600"
+                  >
                     Asics
                   </Link>
                 </li>
                 <li className="my-5">
-                  <Link to="" className="px-[52px] py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600">
+                  <Link
+                    to=""
+                    className="px-[52px] py-1 rounded-lg border border-solid border-black hover:bg-red-600 hover:text-white hover:border-red-600"
+                  >
                     Puma
                   </Link>
                 </li>
@@ -75,16 +90,68 @@ const Product = () => {
             <div className="grid grid-cols-1 md:grid-cols-3 lg:cols-3 gap-16">
               {products.map((product) => {
                 return (
-                  <div key={product.id}>
+                  <div className="card" key={product.id}>
+                    {/* img */}
                     <img
+                      className="img-card p-5"
                       src={product.imageUrl}
                       alt={product.name}
-                      className="w-96"
                     />
-                    <h2 className="text-black hover:underline">
-                      <Link to={`/products/${product.id}`}>{product.name}</Link>
-                    </h2>
+
+                    {/* Tag & Stock  */}
+                    <div className="flex flex-col gap-3 py-3 px-5">
+                      <div className="grid grid-cols-2 items-center gap-3">
+                        {/* 
+                                <div className="text-sm text-end font-semibold line-through text-gray-500">Rp500.000</div>
+                                <div className="text-xl font-semibold text-red-700">60%</div>
+                                    */}
+                        <button className="tag">Adidas</button>
+                        <div className="text-sm items-center text-end font-bold opacity-85">
+                          120 Stock
+                        </div>
+                      </div>
+
+                      <div className="flex flex-col gap-3 mt-3">
+                        {/* Price Name */}
+                        <div className="price">Rp 1.360.000</div>
+                        {/* Product Name */}
+                        <div className="product-title">
+                          <Link className="hover:underline" to={`/products/${product.id}`}>
+                            {product.name}
+                          </Link>
+                        </div>
+                        {/* Tag
+                            <div className="flex items-center gap-2 mt-5">
+                                <button className="tag">
+                                    Adidas
+                                </button>
+                                <button className="tag">
+                                    Low
+                                </button>
+                            </div> */}
+
+                        {/* Atc & Buy Now */}
+                        <div className="grid grid-cols-2 gap-3 mt-8 mb-5">
+                          <button className="primary-button">
+                            Beli Sekarang
+                          </button>
+                          <button className="secondary-button">
+                            Keranjang
+                          </button>
+                        </div>
+                      </div>
+                    </div>
                   </div>
+                  // <div key={product.id}>
+                  //   <img
+                  //     src={product.imageUrl}
+                  //     alt={product.name}
+                  //     className="w-96"
+                  //   />
+                  //   <h2 className="text-black hover:underline">
+                  //     <Link to={`/products/${product.id}`}>{product.name}</Link>
+                  //   </h2>
+                  // </div>
                 );
               })}
             </div>
